54165bd32e200a14ed99cde330a36f64f0e7d1f8
Jairo Martínez <jairomarti@gmail.com>
2026-01-16 13:53:51 -0400

    Proyecto crypto

---
+++ crypto/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Index from './pages/Index'
+
+function App(){
+
+  return (
+    <>
+      <Index />
+       <div className='container'>
+          <h1>Hola Mundo</h1>
+       </div>
+    </>
+  )
+}
+
+export default App
