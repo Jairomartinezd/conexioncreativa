0dec94b100eb62876ce298d2e28d2b20289f6bc7
Jairo Martínez <jairomarti@gmail.com>
2026-01-16 18:58:09 -0400

    modificaciones en index

--- crypto/src/pages/Index.jsx
+++ crypto/src/pages/Index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import coin from 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&names=Bitcoin&symbols=btc&category=layer-1&price_change_percentage=1h'
 import Header from '../components/Header'
-import TM from '../components/tablaMoneda'
+import TablaMoneda from '../components/tablaMoneda'
 
 const Index = () => {
 
@@ -11,7 +11,7 @@ const Index = () => {
         <>
             <div className='container'>
                 <Header />
-                <TM 
+                <TablaMoneda 
                     m={mon} 
                 />
             </div>
